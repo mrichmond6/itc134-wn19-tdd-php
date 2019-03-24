4bf90ebeb4d9404d5fb47cff940ee895101afe75
Morgan Richmond <drawinggravity@gmail.com>
2019-03-24 03:56:14 +0000

    added to test-calc file

--- tdd-test/test-calculator.php
+++ tdd-test/test-calculator.php
@@ -2,6 +2,12 @@
 include 'Calculator.php';
 
 $myCalc = new Calculator();
-echo $myCalc->add(2,5);
+echo "You have added two numbers together to get: ", $myCalc->add(2,5);
+echo "<br>";
+echo "You have added two numbers together to get: ", $myCalc->add(6,13);
+echo "<br>";
+echo "You have added two numbers together to get: ", $myCalc->add(22,8);
+echo "<br>";
+echo "You have added two numbers together to get: ", $myCalc->add(10,1);
 
 ?>
